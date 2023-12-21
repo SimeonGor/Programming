6ff9e09fe8dd1d5cf4ffaf76141eb80b234dbb11
simeongor <simeon.gor@yandex.ru>
2023-12-21 17:33:04 +0300

    upd

--- src/Lab3/Scene.java
+++ src/Lab3/Scene.java
@@ -12,7 +12,9 @@ import java.util.Vector;
 
 public class Scene {
     private Orchestra orchestra;
-    private Shorty main_character;
+    private List<Shorty> malyshi;
+    private List<Shorty> residentsOfZmeevka;
+    private Shorty znayka, nexnayka, solomka, gvozdik, shurupchik, bublik;
     public void init() {
         HarpFactory harpFactory = new SimpleHarpFactory();
         List<Musician> musicianList = new Vector<>();
@@ -21,32 +23,38 @@ public class Scene {
         ));
         for (int i = 0; i < typesOfHarps.size(); i++) {
             Musician t = new Musician("Unnamed", Person.Gender.FEMALE);
-            try {
-                t.takeMusicalInstrument(harpFactory.createHarp("little"));
-            } catch (StealingException e) {
-                System.out.println(e.getMessage());
-            }
-
+            t.takeMusicalInstrument(harpFactory.createHarp("little"));
             musicianList.add(t);
         }
         orchestra = new Orchestra(musicianList.toArray(new Musician[0]));
 
-        main_character = new Shorty("Гвоздик", Person.Gender.MALE);
-        main_character.setHairstyle(Hairstyle.PROTRUDING_TUFT);
+        gvozdik = new Shorty("Гвоздик", Person.Gender.MALE);
+        gvozdik.setHairstyle(Hairstyle.PROTRUDING_TUFT);
+
+        Clothes cleanShirt = new Clean(new FormalWear(FormalWear.Type.SHIRT));
         try {
-            main_character.setClothing(new Clean(new FormalWear(FormalWear.Type.SHIRT)));
+            gvozdik.setClothing(cleanShirt);
         }
         catch (StealingException e) {
-            throw new RuntimeException(e);
+            System.out.println(e.getMessage());
         }
+        System.out.println(gvozdik.getClothing());
+        znayka = new Shorty("Знайка", Person.Gender.MALE);
+        try {
+            znayka.addClothing(cleanShirt);
+        }
+        catch (StealingException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(znayka.getClothing());
     }
 
     public void start() {
-        System.out.println(orchestra);
-        orchestra.play();
-        Place square = new Place("площадка", new Plants(Plants.Type.TREES));
-        square.setTimeOfDay(new Place.TimeOfDay(16, 30));
-
-        main_character.goTo(square);
+//        System.out.println(orchestra);
+//        orchestra.play();
+//        Place square = new Place("площадка", new Plants(Plants.Type.TREES));
+//        square.setTimeOfDay(new Place.TimeOfDay(16, 30));
+//
+//        gvozdik.goTo(square);
     }
 }
