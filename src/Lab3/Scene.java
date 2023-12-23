7299e7ecec78a9019d5e88644e2d979954778827
simeongor <simeon.gor@yandex.ru>
2023-12-23 11:37:40 +0300

    add local classes

--- src/Lab3/Scene.java
+++ src/Lab3/Scene.java
@@ -9,6 +9,7 @@ import Lab3.Thing.MusicalInstrument.Harp;
 import Lab3.Thing.MusicalInstrument.HoldBehavior.HoldInHands;
 import Lab3.Thing.MusicalInstrument.HoldBehavior.HoldOnFloor;
 import Lab3.Thing.MusicalInstrument.HoldBehavior.HoldOnLap;
+import Lab3.Thing.MusicalInstrument.MusicalInstrument;
 import Lab3.Thing.MusicalInstrument.PlayBehavior.PlayCalmly;
 import Lab3.Thing.MusicalInstrument.PlayBehavior.PlayLoudly;
 import Lab3.Thing.MusicalInstrument.PlayBehavior.PlayQuietly;
@@ -17,18 +18,93 @@ import Lab3.Thing.Notebook;
 import Lab3.Thing.StealingException;
 import Lab3.Thing.Thing;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Vector;
+import java.util.*;
 
 public class Scene {
     private Orchestra orchestra;
-    private List<Shorty> malyshi;
+    private List<Shorty> malyshi, malyshki;
     private List<Shorty> residentsOfZmeevka;
     private Shorty znayka, neznayka, solomka, gvozdik, shurupchik, bublik;
 
     private Town greenTown, flowerTown, zmeevka;
     public void init() {
+        interface ShortyFactory {
+            Shorty createShorty();
+        }
+
+        class SimpleMusicianFactory implements ShortyFactory {
+            private String name;
+            private Person.Gender gender;
+            private Town nativeTown;
+            private MusicalInstrument musicalInstrument;
+
+            public SimpleMusicianFactory(String name, Person.Gender gender, Town nativeTown, MusicalInstrument musicalInstrument) {
+                this.name = name;
+                this.gender = gender;
+                this.nativeTown = nativeTown;
+                this.musicalInstrument = musicalInstrument;
+            }
+
+
+            @Override
+            public Musician createShorty() {
+                Musician musician = new Musician("Unnamed", Person.Gender.FEMALE, greenTown);
+                try {
+                    musician.takeMusicalInstrument(musicalInstrument);
+                }
+                catch (StealingException e) {
+                    System.out.println(e.getMessage());
+                }
+
+                return musician;
+            }
+        }
+        class SimpleShortyFactory implements ShortyFactory {
+            private String name;
+            private Person.Gender gender;
+            private Hairstyle hairstyle;
+            private Town nativeTown;
+            private Map<String, Clothes> clothes;
+            private Map<String, Thing> thing;
+            public SimpleShortyFactory(String name,
+                                       Person.Gender gender,
+                                       Town nativeTown, Hairstyle hairstyle,
+                                       Map<String, Clothes> clothes, Map<String, Thing> thing) {
+                this.name = name;
+                this.gender = gender;
+                this.nativeTown = nativeTown;
+                this.hairstyle = hairstyle;
+                this.clothes = clothes;
+                this.thing = thing;
+            }
+            @Override
+            public Shorty createShorty() {
+                Shorty shorty = new Shorty(name, gender, nativeTown);
+                shorty.setHairstyle(hairstyle);
+                if (clothes != null) {
+                    for (var e : clothes.entrySet()) {
+                        try {
+                            shorty.setClothing(e.getKey(), e.getValue());
+                        } catch (StealingException ex) {
+                            System.out.println(ex.getMessage());
+                        }
+                    }
+                }
+
+                if (thing != null) {
+                    for (var e : thing.entrySet()) {
+                        try {
+                            shorty.takeThing(e.getKey(), e.getValue());
+                        } catch (StealingException ex) {
+                            System.out.println(ex.getMessage());
+                        }
+                    }
+                }
+
+                return shorty;
+            }
+
+        }
         greenTown = new Town("Green Town");
         Place square = new Place("площадка", new Plants(Plants.Type.TREES));
         Place dandelionThickets = new Place("заросли одуванчиков", new Plants(Plants.Type.FLOWERS));
@@ -57,72 +133,107 @@ public class Scene {
                 new String[]{"little", "little", "middle", "middle", "big", "huge"}
         ));
         for (int i = 0; i < typesOfHarps.size(); i++) {
-            Musician t = new Musician("Unnamed", Person.Gender.FEMALE, greenTown);
-            try {
-                t.takeMusicalInstrument(harpFactory.createHarp("little"));
-            }
-            catch (StealingException e) {
-                System.out.println(e.getMessage());
-            }
-            musicianList.add(t);
+            musicianList.add((new SimpleMusicianFactory("Unnamed",
+                    Person.Gender.FEMALE,
+                    greenTown,
+                    harpFactory.createHarp(typesOfHarps.get(i)))).createShorty());
         }
         orchestra = new Orchestra(musicianList.toArray(new Musician[0]));
 
-        gvozdik = new Shorty("Гвоздик", Person.Gender.MALE, zmeevka);
-        gvozdik.setHairstyle(Hairstyle.PROTRUDING_TUFT);
-        try {
-            gvozdik.setClothing("SHIRT", new Clean(new FormalWear(FormalWear.Type.SHIRT)));
-        }
-        catch (StealingException e) {
-            System.out.println(e.getMessage());
-        }
+        Map<String, Clothes> znaykaClothes = new HashMap<String, Clothes>();
+        znaykaClothes.put("SUIT", new Ironed(new FormalWear(FormalWear.Type.SUIT)));
+        Map<String, Thing> znaykaThings = new HashMap<>();
+        znaykaThings.put("notebook", new Notebook(10));
 
-        znayka = new Shorty("Знайка", Person.Gender.MALE, flowerTown);
-        try {
-            znayka.setClothing("SUIT", new Ironed(new FormalWear(FormalWear.Type.SUIT)));
-        } catch (StealingException e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            znayka.takeThing("notebook", new Notebook(10));
-        } catch (StealingException e) {
-            System.out.println(e.getMessage());
-        }
+        znayka = (new SimpleShortyFactory("Знайка",
+                Person.Gender.MALE,
+                flowerTown,
+                Hairstyle.OTHER,
+                znaykaClothes,
+                znaykaThings)).createShorty();
 
-        solomka = new Shorty("Соломка", Person.Gender.FEMALE, greenTown);
-        try {
-            solomka.setClothing("dress", new Clean(new WomenClothing(WomenClothing.Type.DRESS)));
-        } catch (StealingException e) {
-            System.out.println(e.getMessage());
-        }
+        Map<String, Clothes> gvozdikClothes = new HashMap<>();
+        gvozdikClothes.put("SHIRT", new Clean(new FormalWear(FormalWear.Type.SHIRT)));
 
-        neznayka = new Shorty("Незнайка", Person.Gender.MALE, flowerTown);
-        try {
-            neznayka.setClothing("SHIRT", new Wrinkled(new FormalWear(FormalWear.Type.SHIRT)));
-        } catch (StealingException e) {
-            System.out.println(e.getMessage());
-        }
+        gvozdik = (new SimpleShortyFactory("Гвоздик",
+                Person.Gender.MALE,
+                zmeevka,
+                Hairstyle.PROTRUDING_TUFT,
+                gvozdikClothes,
+                null)).createShorty();
 
+        Map<String, Clothes> solomkaClothes = new HashMap<>();
+        solomka = (new SimpleShortyFactory("Соломка",
+                Person.Gender.FEMALE,
+                greenTown,
+                Hairstyle.PIGTAIL,
+                solomkaClothes,
+                null)).createShorty();
+
+        Map<String, Clothes> neznaykaClothes = new HashMap<>();
+        neznaykaClothes.put("SHIRT", new Wrinkled(new FormalWear(FormalWear.Type.SHIRT)));
+        neznayka = (new SimpleShortyFactory("Незнайка",
+                Person.Gender.MALE,
+                flowerTown,
+                Hairstyle.MESSY,
+                neznaykaClothes,
+                null)).createShorty();
+
+        malyshki = new Vector<>();
+        for (int i = 0; i < 5; i++) {
+            malyshki.add((new SimpleShortyFactory(
+                    "Unnamed" + i,
+                    Person.Gender.FEMALE,
+                    greenTown,
+                    Hairstyle.PIGTAIL,
+                    null,
+                    null)).createShorty());
+        }
         malyshi = new Vector<>();
         for (int i = 0; i < 5; i++) {
-            malyshi.add(new Shorty("Unnamed" + i, Person.Gender.FEMALE, greenTown));
+            malyshi.add((new SimpleShortyFactory(
+                    "Unnamed" + i,
+                    Person.Gender.MALE,
+                    greenTown,
+                    Hairstyle.OTHER,
+                    null,
+                    null)).createShorty());
         }
 
-        shurupchik = new Shorty("Шурупчик", Person.Gender.MALE, zmeevka);
-        bublik = new Shorty("Бублик", Person.Gender.MALE, zmeevka);
+
+        shurupchik = (new SimpleShortyFactory("Шурупчик",
+                Person.Gender.MALE,
+                zmeevka,
+                Hairstyle.SHORT,
+                null,
+                null)).createShorty();
+
+        bublik = (new SimpleShortyFactory("Бублик",
+                Person.Gender.MALE,
+                zmeevka,
+                Hairstyle.MESSY,
+                null,
+                null)).createShorty();
 
         residentsOfZmeevka = new Vector<>();
         for (int i = 0; i < 10; i++) {
-            residentsOfZmeevka.add(new Shorty("Unnamed" + i,
+            residentsOfZmeevka.add((new SimpleShortyFactory("Unnamed" + i,
                     (i % 2 == 0) ? Person.Gender.MALE : Person.Gender.FEMALE,
-                    zmeevka));
+                    zmeevka,
+                    Hairstyle.OTHER,
+                    null,
+                    null)).createShorty());
         }
+
+
+        System.out.println("\n\n\n\n");
     }
 
     public void start() {
+        System.out.println("\t\tMain Scene\n\n");
         znayka.goTo(greenTown.getPlaces().get("field"));
         Speech storyOfSolomka = new BasicSpeech("Story of " + solomka);
-        for (var e: malyshi) {
+        for (var e: malyshki) {
             e.speak(storyOfSolomka);
         }
 
